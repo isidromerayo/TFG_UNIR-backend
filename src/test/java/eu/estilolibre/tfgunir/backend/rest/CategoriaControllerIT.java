823c33f1b4e31b5e8147bb426c1ece6b5cdaf5c7
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2023-06-11 17:42:23 +0200

    failsafe plugin para test de integración

---
+++ src/test/java/eu/estilolibre/tfgunir/backend/rest/CategoriaControllerIT.java
@@ -0,0 +1,39 @@
+package eu.estilolibre.tfgunir.backend.rest;
+
+
+import org.aspectj.lang.annotation.Before;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+
+import io.restassured.RestAssured;
+
+import static io.restassured.RestAssured.given;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CategoriaControllerIT {
+
+    private final static String BASE_URI = "http://localhost";
+ 
+    @LocalServerPort
+    private int port;
+ 
+    @Before(value = "")
+    public void configureRestAssured() {
+        RestAssured.baseURI = BASE_URI;
+        RestAssured.port = port;
+    }
+
+    @Disabled("Pendiente de encontrar ejemplo con Spring Boot 3")
+    public void categoriasAPIStatus() {
+        given().
+                when().
+                get("/api/categorias").
+                then().
+                log().all().
+                assertThat().
+                statusCode(200);
+    }
+}
