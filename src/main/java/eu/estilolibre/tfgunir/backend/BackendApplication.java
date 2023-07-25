ece20bccf8fcaedfd3632b83a2c897fd481a57e8
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2023-07-25 09:55:07 +0200

    Filtro de cors para llamada desde el frontend básico

--- src/main/java/eu/estilolibre/tfgunir/backend/BackendApplication.java
+++ src/main/java/eu/estilolibre/tfgunir/backend/BackendApplication.java
@@ -2,6 +2,10 @@ package eu.estilolibre.tfgunir.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,4 +14,20 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		// TODO - Lambda DSL
+		http
+				.csrf().disable()
+				.authorizeHttpRequests(requests -> requests
+						.requestMatchers(HttpMethod.GET, "/**").permitAll()
+						.requestMatchers(HttpMethod.POST, "/api/**").permitAll()
+						.requestMatchers(HttpMethod.GET, "/api/*").authenticated()
+						.requestMatchers(HttpMethod.POST, "/api/*").authenticated()
+						.requestMatchers(HttpMethod.PUT, "/api/*").authenticated()
+						.requestMatchers(HttpMethod.DELETE, "/api/*").authenticated()
+						.anyRequest().denyAll())
+				.httpBasic();
+		return http.build();
+	}
 }
