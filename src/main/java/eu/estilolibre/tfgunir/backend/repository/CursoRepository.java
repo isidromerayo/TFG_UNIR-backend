8be888d75fcfda8e2ea46a77c6f6225db2d506b7
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2023-07-25 17:26:04 +0200

    API expone ids

--- src/main/java/eu/estilolibre/tfgunir/backend/repository/CursoRepository.java
+++ src/main/java/eu/estilolibre/tfgunir/backend/repository/CursoRepository.java
@@ -2,11 +2,15 @@ package eu.estilolibre.tfgunir.backend.repository;
 
 import java.util.List;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import eu.estilolibre.tfgunir.backend.model.Curso;
 
@@ -25,4 +29,13 @@ public interface CursoRepository extends PagingAndSortingRepository<Curso, Long>
      */
     @Query("select c from Curso c order by c.fechaActualizacion desc limit 3")
     List<Curso> selectLastUpdates();
+    
+    @Configuration
+    static class RepositoryConfig implements RepositoryRestConfigurer {
+        @Override
+        public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
+                CorsRegistry corsRegistry) {
+            config.exposeIdsFor(Curso.class);
+        }
+    }
 }
