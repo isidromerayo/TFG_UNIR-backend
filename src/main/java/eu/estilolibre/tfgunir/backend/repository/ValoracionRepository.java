3423e39d4e05a331664bb5d470f8d03a6a7496a5
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2023-07-28 11:27:29 +0200

    página de búsqueda, pendiente formulario de búsqueda

--- src/main/java/eu/estilolibre/tfgunir/backend/repository/ValoracionRepository.java
+++ src/main/java/eu/estilolibre/tfgunir/backend/repository/ValoracionRepository.java
@@ -2,11 +2,16 @@ package eu.estilolibre.tfgunir.backend.repository;
 
 import java.util.List;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
+import eu.estilolibre.tfgunir.backend.model.Usuario;
 import eu.estilolibre.tfgunir.backend.model.Valoracion;
 
 @CrossOrigin(origins = {"http://localhost:4200","http://localhost:3000","http://localhost:5173"})
@@ -19,6 +24,15 @@ public interface ValoracionRepository extends  JpaRepository<Valoracion, Long> {
      * 
      * @return
      */
-    @Query("select v from Valoracion v order by v.fecha desc limit 3")
+    @Query("select v from Valoracion v inner join v.estudiante e  order by v.fecha desc limit 3")
     List<Valoracion> selectLastOpinions();
+
+    @Configuration
+    static class RepositoryConfig implements RepositoryRestConfigurer {
+        @Override
+        public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config,
+                CorsRegistry corsRegistry) {
+            config.exposeIdsFor(Valoracion.class);
+        }
+    }
 }
