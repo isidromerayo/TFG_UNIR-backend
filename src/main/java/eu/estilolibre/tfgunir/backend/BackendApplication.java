e6264bde913f22ae15b2e323bdd8814790a78dec
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2023-07-28 09:51:12 +0200

    registro de usuario (problemas de configuración con el POST en backend, se desactiva)

--- src/main/java/eu/estilolibre/tfgunir/backend/BackendApplication.java
+++ src/main/java/eu/estilolibre/tfgunir/backend/BackendApplication.java
@@ -2,10 +2,12 @@ package eu.estilolibre.tfgunir.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+ */
 
 @SpringBootApplication
 public class BackendApplication {
@@ -13,21 +15,15 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
+	/*
 	@Bean
 	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-		// TODO - Lambda DSL
-		http
-				.csrf().disable()
-				.authorizeHttpRequests(requests -> requests
-						.requestMatchers(HttpMethod.GET, "/**").permitAll()
-						.requestMatchers(HttpMethod.POST, "/api/**").permitAll()
-						.requestMatchers(HttpMethod.GET, "/api/*").authenticated()
-						.requestMatchers(HttpMethod.POST, "/api/*").authenticated()
-						.requestMatchers(HttpMethod.PUT, "/api/*").authenticated()
-						.requestMatchers(HttpMethod.DELETE, "/api/*").authenticated()
-						.anyRequest().denyAll())
-				.httpBasic();
+		http.csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.disable())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/*").permitAll();
+                });
 		return http.build();
 	}
+	*/ 
 }
