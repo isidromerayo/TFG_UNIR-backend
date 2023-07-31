b069ff1843ebd29b8fa23b44e713df2ed336698f
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2023-07-31 18:59:10 +0200

    crear y añadir test simple

---
+++ src/test/java/eu/estilolibre/tfgunir/backend/repository/UsuarioRepositoryTests.java
@@ -0,0 +1,26 @@
+package eu.estilolibre.tfgunir.backend.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import eu.estilolibre.tfgunir.backend.model.Usuario;
+
+import java.util.List;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest
+public class UsuarioRepositoryTests {
+ 
+    @Autowired
+    UsuarioRepository repository;
+
+    @Test
+    void buscarTodosLasUsuarios() {
+        List<Usuario> result = (List<Usuario>)repository.findAll();
+        assertThat(result.size(),is(equalTo(8)));
+    }
+
+}
