44ae1209f8c32da77bbe0e3533187c532409cf8e
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2025-03-11 19:40:59 +0100

    fix job

--- src/main/java/com/unir/tfg/config/WebConfig.java
+++ src/main/java/com/unir/tfg/config/WebConfig.java
@@ -11,8 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:4200",
-            "http://localhost:3000",
-            "http://localhost:5173")
+                    "http://localhost:3000",
+                    "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
