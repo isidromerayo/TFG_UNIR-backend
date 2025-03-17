27ef7e1fc964af74dd5a6a0264922c02722f4c44
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2025-03-17 22:32:11 +0100

    migrar a versión superior de Java JWT (io.jsonwebtoken)

--- src/main/java/eu/estilolibre/tfgunir/backend/security/TokenService.java
+++ src/main/java/eu/estilolibre/tfgunir/backend/security/TokenService.java
@@ -6,31 +6,31 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import java.util.Date;
+import java.util.logging.Logger;
 
 public class TokenService {
- 
+    
+    private static final Logger logger = Logger.getLogger(TokenService.class.getName());
+
     public String crearToken(String usuario, String claveEncriptar, Date expiracion) {
         SecretKey clave = Keys.hmacShaKeyFor(claveEncriptar.getBytes());
         
-        String JWT = Jwts.builder()
-        .setSubject(usuario)
-        .setExpiration(expiracion)
-        .signWith(clave,SignatureAlgorithm.HS256)
+    return Jwts.builder()
+        .claim("sub", usuario)
+        .expiration(expiracion)
+        .signWith(clave)
         .compact();
-   
-        return JWT;
     }
 
     public String leerToken(String tokenReal, String claveEncriptar) {
         SecretKey clave = Keys.hmacShaKeyFor(claveEncriptar.getBytes());
 
-        JwtParser parser = Jwts.parserBuilder().setSigningKey(clave).build();
-
-        Jws<Claims> token = parser.parseClaimsJws(tokenReal);
-        System.out.println(token.getHeader() + "/" + token.getBody().toString() + "/" + token.getSignature());
+        JwtParser parser = Jwts.parser().verifyWith(clave).build();
+        
+        Jws<Claims> token = parser.parseSignedClaims(tokenReal);
+        logger.info(token.getHeader() + "/" + token.getBody().toString() + "/" + token.getSignature());
         return token.getBody().getSubject();
     }
 }
