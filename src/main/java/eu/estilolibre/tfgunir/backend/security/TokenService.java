afdcd43d7d9eb226bc2e0794c1e09e7fae2b125c
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2025-05-06 20:53:48 +0200

    formateo

--- src/main/java/eu/estilolibre/tfgunir/backend/security/TokenService.java
+++ src/main/java/eu/estilolibre/tfgunir/backend/security/TokenService.java
@@ -11,28 +11,28 @@ import java.util.Date;
 import java.util.logging.Logger;
 
 public class TokenService {
-    
+
     private static final Logger logger = Logger.getLogger(TokenService.class.getName());
 
     public String crearToken(String usuario, String claveEncriptar, Date expiracion) {
         SecretKey clave = Keys.hmacShaKeyFor(claveEncriptar.getBytes());
-        
-    return Jwts.builder()
-        .claim("sub", usuario)
-        .expiration(expiracion)
-        .signWith(clave)
-        .compact();
+
+        return Jwts.builder()
+                .claim("sub", usuario)
+                .expiration(expiracion)
+                .signWith(clave)
+                .compact();
     }
 
     public String leerToken(String tokenReal, String claveEncriptar) {
         SecretKey clave = Keys.hmacShaKeyFor(claveEncriptar.getBytes());
 
         JwtParser parser = Jwts.parser().verifyWith(clave).build();
-        
+
         Jws<Claims> token = parser.parseSignedClaims(tokenReal);
         String[] parts = tokenReal.split("\\.");
-String signature = (parts.length == 3) ? parts[2] : "";
-logger.info(token.getHeader() + "/" + token.getPayload().toString() + "/" + signature);
+        String signature = (parts.length == 3) ? parts[2] : "";
+        logger.info(token.getHeader() + "/" + token.getPayload().toString() + "/" + signature);
         return token.getPayload().getSubject();
     }
 }
