85144005adbc9eb26c89edafb5908a0985869810
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2025-12-19 21:34:41 +0100

    fix:  JUnit5 test classes and methods should have default package visibility java:S5786

--- src/test/java/eu/estilolibre/tfgunir/backend/repository/UsuarioRepositoryTests.java
+++ src/test/java/eu/estilolibre/tfgunir/backend/repository/UsuarioRepositoryTests.java
@@ -14,7 +14,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 @DataJpaTest
 @ActiveProfiles("test")
-public class UsuarioRepositoryTests {
+class UsuarioRepositoryTests {
  
     @Autowired
     UsuarioRepository repository;
