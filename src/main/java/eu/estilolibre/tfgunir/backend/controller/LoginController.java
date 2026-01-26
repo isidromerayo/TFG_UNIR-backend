165d9e9dd435fb1123203f0dc6774918eddce141
Isidro Merayo Castellano <isidro.merayo@gmail.com>
2026-01-26 20:07:55 +0100

    fix: drop redundant annotations

--- src/main/java/eu/estilolibre/tfgunir/backend/controller/LoginController.java
+++ src/main/java/eu/estilolibre/tfgunir/backend/controller/LoginController.java
@@ -2,7 +2,6 @@ package eu.estilolibre.tfgunir.backend.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -31,7 +30,6 @@ public class LoginController {
     private final PasswordEncoder passwordEncoder;
     private final TokenService tokenService;
 
-    @Autowired
     public LoginController(
             UsuarioRepository repository,
             PasswordEncoder passwordEncoder,
--- src/main/java/eu/estilolibre/tfgunir/backend/security/WebConfig.java
+++ src/main/java/eu/estilolibre/tfgunir/backend/security/WebConfig.java
@@ -1,7 +1,6 @@
 package eu.estilolibre.tfgunir.backend.security;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(@NonNull CorsRegistry registry) {
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
                 .allowedOrigins("http://localhost:4200",
                     "http://localhost:3000",
